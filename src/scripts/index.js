ff1d96aa9559ad40d82ec6b6996997f2dc6fc6c2
shafikul1170 <shafikul1170@gmail.com>
2021-04-28 01:58:19 +0600

    3nd commit

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,9 +1,26 @@
 const URL = 'https://jsonplaceholder.typicode.com/users'
 
-const xhr = new XMLHttpRequest();
+// @ XMLHttpRequest
 
-xhr.onreadystatechange =() => {
-    console.log(xhr.response)
-}
-xhr.open('GET', URL)
-xhr.send();
+// let btn = document.querySelector('#loadData')
+// let p = document.querySelector('#output')
+
+// btn.addEventListener('click', function () {
+//     const xhr = new XMLHttpRequest();
+
+//     xhr.onreadystatechange = () => {
+//         p.innerHTML = xhr.response
+//     }
+//     xhr.open('GET', URL)
+//     xhr.send()
+// });
+
+//fetch api
+let btn = document.querySelector('#loadData')
+let p = document.querySelector('#output')
+
+fetch(URL)
+    // .then(res => console.log(res.json()))
+    .then(res => res.json())
+    .then(data => console.log(data))
+    .catch(err => console.log(err))
